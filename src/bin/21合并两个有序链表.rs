4af4b0e34ab532bdd59e3ef21a84ddf72fdf4c14
yin <yin_wb@qq.com>
2025-04-07 00:31:45 +0800

    提交简单题21、26、27、28、35

--- src/bin/21合并两个有序链表.rs
+++ src/bin/21合并两个有序链表.rs
@@ -13,41 +13,41 @@
  * Total Submissions: 3M
  * Testcase Example:  '[1,2,4]\n[1,3,4]'
  *
- * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
- * 
- * 
- * 
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：l1 = [1,2,4], l2 = [1,3,4]
  * 输出：[1,1,2,3,4,4]
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：l1 = [], l2 = []
  * 输出：[]
- * 
- * 
+ *
+ *
  * 示例 3：
- * 
- * 
+ *
+ *
  * 输入：l1 = [], l2 = [0]
  * 输出：[0]
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 两个链表的节点数目范围是 [0, 50]
- * -100 
+ * -100
  * l1 和 l2 均按 非递减顺序 排列
- * 
- * 
+ *
+ *
  */
 
 fn main() {}
@@ -55,18 +55,15 @@ struct Solution;
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
 }
 
 impl ListNode {
-  #[inline]
-  fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
     }
-  }
 }
 
 // @lc code=start
@@ -76,7 +73,7 @@ impl ListNode {
 //   pub val: i32,
 //   pub next: Option<Box<ListNode>>
 // }
-// 
+
 // impl ListNode {
 //   #[inline]
 //   fn new(val: i32) -> Self {
@@ -87,10 +84,35 @@ impl ListNode {
 //   }
 // }
 impl Solution {
-    pub fn merge_two_lists(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        
-        unimplemented!()
+    pub fn merge_two_lists(
+        mut list1: Option<Box<ListNode>>,
+        mut list2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let mut head = None;
+        let mut cur = &mut head;
+
+        loop {
+            match (list1, list2) {
+                (Some(mut l1), Some(mut l2)) => {
+                    if l1.val < l2.val {
+                        list1 = l1.next.take();
+                        list2 = Some(l2);
+                        *cur = Some(l1);
+                    } else {
+                        list1 = Some(l1);
+                        list2 = l2.next.take();
+                        *cur = Some(l2);
+                    }
+                }
+                (None, Some(l)) | (Some(l), None) => {
+                    *cur = Some(l);
+                    break;
+                },
+                (None, None) => break,
+            }
+        }
+
+        head
     }
 }
 // @lc code=end
-
---
+++ src/bin/28找出字符串中第一个匹配项的下标.rs
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=rust
+ *
+ * [28] 找出字符串中第一个匹配项的下标
+ *
+ * https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+ *
+ * algorithms
+ * Easy (43.81%)
+ * Likes:    2392
+ * Dislikes: 0
+ * Total Accepted:    1.3M
+ * Total Submissions: 2.8M
+ * Testcase Example:  '"sadbutsad"\n"sad"'
+ *
+ * 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0
+ * 开始）。如果 needle 不是 haystack 的一部分，则返回  -1 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：haystack = "sadbutsad", needle = "sad"
+ * 输出：0
+ * 解释："sad" 在下标 0 和 6 处匹配。
+ * 第一个匹配项的下标是 0 ，所以返回 0 。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：haystack = "leetcode", needle = "leeto"
+ * 输出：-1
+ * 解释："leeto" 没有在 "leetcode" 中出现，所以返回 -1 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= haystack.length, needle.length <= 10^4
+ * haystack 和 needle 仅由小写英文字符组成
+ *
+ *
+ */
+
+fn main() {}
+struct Solution;
+
+// @lc code=start
+impl Solution {
+    pub fn str_str(haystack: String, needle: String) -> i32 {
+        haystack.find(&needle).unwrap_or(usize::MAX) as i32
+    }
+}
+// @lc code=end
